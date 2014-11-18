efa078f9433cbbab53ee0aaf17784909d2747251
maruxyz <maruxyz@gmail.com>
2014-11-18 11:05:52 +0900

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    @user = Hash.new
+    @user[:name] = "name";
+    @user[:username] = params[:username]
+    @user[:location] = "location"
+    @user[:about] = "about"
+    @user["about"] = "about2"
+  end
+end
